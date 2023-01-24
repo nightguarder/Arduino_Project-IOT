169c30ae6dca92c8cac6d03b1d27a741f4831673
nightguarder <nigthguarder@gmail.com>
2023-01-24 13:52:41 +0100

    Arduino.ino sketch first commit.

---
+++ arduino/IoT-Project_main/IoT-Project_main.ino
@@ -0,0 +1,14 @@
+#include <WiFiNINA.h> //Official WiFiNINA library for all boards Author: Arduino
+
+#include <SPI.h> //Preinstalled library for Arduino Author: Arduino
+#include <PubSubClient.h> //A client library for MQTT messaging. Author: Nick O'Leary
+#include "arduino_secrets.h" //Secrets file for Arduino Author: Cyril S.
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ arduino/IoT-Project_main/secrets_example.h
@@ -0,0 +1,10 @@
+//You should never share your password publicly
+// Therefore I recommend to create your own file and store it safely.
+#ifndef ARDUINO_SECRETS_H
+#define ARDUINO_SECRETS_H
+
+#define SECRET_SSID "Your SSID"
+#define SECRET_PASS "Your_password"
+
+#endif
+
